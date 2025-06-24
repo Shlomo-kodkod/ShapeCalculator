adc44d57c50ecf62323ce8e6e48a81f5e7d56eb1
kodcode <quarts.91.elders@icloud.com>
2025-06-24 18:48:18 +0300

    add files

--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -1,20 +1,21 @@
 from shape import Shape
 
 class Rectangle(Shape):
-    def __init__(self, side_a, side_b):
-        self.__shape = "Rectangle"
-        self.__side1 = side_a
-        self.__side2 = side_b
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
     
     def area(self):
-        return self.__side1 * self.__side2
+        return self.__width * self.__height
     
     def perimeter(self):
-        return (self.__side1 * 2) + (self.__side2 * 2)
+        return (self.__width * 2) + (self.__height * 2)
     
     def __str__(self):
-        return f"Shape: {self.__shape}, \nArea: {self.area()}, \nPerimeter: {self.perimeter()}"
+        return f"Rectangle of width {self.__width} and height {self.__height}, Area: {self.area()}, Perimeter: {self.perimeter()}"
     
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
 
     
 
