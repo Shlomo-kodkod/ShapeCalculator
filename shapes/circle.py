9eee3a6fe6add94041de24f949cc6686f8a5f1e6
kodcode <quarts.91.elders@icloud.com>
2025-06-25 17:03:14 +0300

    finish

--- shapes/circle.py
+++ shapes/circle.py
@@ -2,6 +2,7 @@ from shapes.shape import Shape
 import math
 
 class Circle(Shape):
+    
     def __init__(self, radius):
         self.name = "Circle"
         self.__radius = radius
@@ -13,11 +14,9 @@ class Circle(Shape):
     def area(self):
         return (self.radius ** 2) * math.pi
         
-    
     def perimeter(self):
         return self.radius * (math.pi * 2)
         
-    
     def __str__(self):
         return f"{self.name} of radius {self.radius}. \nArea: {self.area()}. \nPerimeter: {self.perimeter()}.\n"
     
